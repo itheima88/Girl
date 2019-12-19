666fea14059d99509f5460501d9d2bb2ca780b5b
itheima <466304981@qq.com>
2019-12-19 17:21:21 +0800

    貂蝉

--- src/main/java/com/itheima/ss/Sun.java
+++ src/main/java/com/itheima/ss/Sun.java
@@ -3,5 +3,7 @@ package com.itheima.ss;
 public class Sun {
     public static void main(String[] args) {
         System.out.println("妲己..好骚..");
+
+        System.out.println("貂蝉也好骚...");
     }
 }
